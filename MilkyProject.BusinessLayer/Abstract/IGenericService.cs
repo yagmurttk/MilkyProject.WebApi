db48e894674f00ff8b60b76a4ce840308c0cddd4
Yağmur K. <yagmurrttk@gmail.com>
2024-04-23 16:22:47 +0300

    Add project files.

---
+++ MilkyProject.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkyProject.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        void TInsert(T entity);
+        void TDelete(int id);
+        void TUpdate(T entity);
+        List<T> TGetListAll();
+        T TGetById(int id);
+    }
+}
+}
---
+++ MilkyProject.DataAccessLayer/Context/MilkyContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MilkyProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkyProject.DataAccessLayer.Context
+{
+    public class MilkyContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-C1IT2N4\\MSSQLSERVER02;initial Catalog=DbMilky;integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; } 
+        public DbSet<Product> Products { get; set; } 
+        public DbSet<Slider> Sliders { get; set; } 
+    }
+}
